dea564d4d80d3de5a9c11fa60edfe23f34f88481
Ben Eriksson <ben@cmnatic.co.uk>
2020-02-15 14:57:56 +0000

    Working Registration

---
+++ DashboardPage/dashboard.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ben Eriksson">
+
+    <title>I've CovsIT</title>
+	<!-- custom css styles -->
+	<link href="../css/styles.css" rel="stylesheet">
+
+    <!-- Ionicons styles -->
+  <link href="../css/ionicons.min.css" rel="stylesheet">
+
+
+  <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+
+
+	<!-- Bootstrap Library -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" >
+
+    <!-- Javacript & Jquery -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<title>I've Covs-IT</title>
+<nav class="navbar bg-dark navbar-dark navbar-expand-lg fixed-top">
+   <div class="container">
+       <a class="navbar-brand" href="index.html"><img src="../imgs/logo.png" width="64" height="64"><!--<i class="ionicons ion-android-bicycle"></i>-->I've CovsIT</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+       <span class="navbar-toggler-icon"></span></button>
+
+    <!-- Hyperlink to the 'About' section of the page -->
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="navbar-item active">
+          <a class="nav-link" href="../index.html">About</a>
+        </li>
+
+        <li class="navbar-item">
+          <a class="nav-link" href="../RegistrationPage/registration.html">Register</a>
+        </li>
+
+        <li class="navbar-item">
+          <a class="nav-link" href="../LoginPage/login.html">Login</a>
+        </li>
+
+        <li class="navbar-item">
+          <a class="nav-link" href="../ContactUs/contactus.html">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</nav>
+
+<h1> Successful Login! </h1>
+<script type="text/javascript">
+document.write(document.cookie);
+
+</script>
+
+</body>
+</html>
